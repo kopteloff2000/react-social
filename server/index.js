cdb65b54481f3b33adc5b8428eba8657dd410962
Nikita <79460691+kopteloff2000@users.noreply.github.com>
2021-09-03 17:58:13 +0500

    Added server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const helmet = require("helmet")
+const morgan = require("morgan")
+
+const app = express();
+
+dotenv.config();
+
+app.listen(8800, ()=>{
+    console.log("all right")
+})
